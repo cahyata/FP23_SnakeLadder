7894e08c105706b2d63ae6abd82813fb4de72db2
jerji <guardbros06@gmail.com>
2025-11-17 11:26:21 +0700

    base

---
+++ src/java/Edge.java
@@ -0,0 +1,18 @@
+
+
+// Edge class representing a connection between nodes
+class Edge {
+    private Node source;
+    private Node target;
+    private int weight;
+
+    public Edge(Node source, Node target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
+
+    public Node getSource() { return source; }
+    public Node getTarget() { return target; }
+    public int getWeight() { return weight; }
+}
